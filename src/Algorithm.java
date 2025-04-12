ad73318352ae6970997f14f8de854085801c2ef9
schhidal23 <chhidalsandip@gmail.com>
2025-04-12 23:25:01 +0200

    New class Algorithm has been created.

---
+++ src/Algorithm.java
@@ -0,0 +1,21 @@
+public class Algorithm {
+    int add(int number1, int number2){   //Add two numbers
+        int addition = number1 + number2;
+        return addition;
+     }
+ 
+     int sub(int number1, int number2){
+         int substraction = number1 - number2;
+         return substraction;
+     }
+     int multi(int number1, int number2){
+         int multiplication = number1 * number2;
+         return multiplication;
+     }
+     int div(int number1, int number2){
+         int division = number1 / number2;
+         return division;
+ 
+     }
+
+}
