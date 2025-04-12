8263800e924e60caba3bd2b568111c859fffa117
schhidal23 <chhidalsandip@gmail.com>
2025-04-13 00:50:25 +0200

    loops practice

---
+++ src/test/Loops.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        String[] cars = { "Audi", "Mercedez", "Opel", "BMW", "Volkswagen" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+    }
+
+}
