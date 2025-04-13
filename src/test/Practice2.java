49532f4cd371c6a8522aadd44d1b6b13b3cf04a6
schhidal23 <chhidalsandip@gmail.com>
2025-04-13 13:41:57 +0200

    NEw class created

---
+++ src/test/Practice2.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        
+        String[] names = new String[4];
+        names[0] = "Sandip";
+        names[1] = "Anup";
+        names[2] = "Hari";
+        names[3] = "Nabin";
+        // names[4] = "Sam";
+
+        for (String i:names){
+            System.out.println(i); 
+        }
+
+       
+    }
+
+}
